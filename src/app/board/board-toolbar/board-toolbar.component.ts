c6ed2661d3cf01832e6e1eab91144173aa59dda9
vincenzorispoli.vr@gmail.com <you@example.com>
2024-05-15 11:55:57 +0200

    logo added

---
+++ src/app/board/board-toolbar/board-toolbar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-board-toolbar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './board-toolbar.component.html',
+  styleUrl: './board-toolbar.component.scss'
+})
+export class BoardToolbarComponent {
+
+}
--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BoardToolbarComponent } from './board-toolbar/board-toolbar.component';
 
 @Component({
   selector: 'app-board',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, BoardToolbarComponent],
   templateUrl: './board.component.html',
   styleUrl: './board.component.scss'
 })
