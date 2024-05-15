bc5c86d0e707efc2815c1cad9f883e94ba69d06a
Tobias Roeske <122990092+tobiasroeske@users.noreply.github.com>
2024-05-15 10:29:44 +0200

    login and board page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { BoardComponent } from './board/board.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: LandingPageComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'board', component: BoardComponent},
+
+];
