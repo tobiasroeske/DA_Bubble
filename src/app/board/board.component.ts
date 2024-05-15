7d307f498d465a91d25a381cac4033832e11880a
vincenzorispoli.vr@gmail.com <you@example.com>
2024-05-15 15:46:08 +0200

    new code and folder icon created

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BoardToolbarComponent } from './board-toolbar/board-toolbar.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
 
 @Component({
   selector: 'app-board',
   standalone: true,
-  imports: [CommonModule, BoardToolbarComponent],
+  imports: [CommonModule, BoardToolbarComponent, SidenavComponent],
   templateUrl: './board.component.html',
   styleUrl: './board.component.scss'
 })
