f35b4aa07fb7d62e95aa26fd579905788285aaf1
vincenzorispoli.vr@gmail.com <you@example.com>
2024-05-17 14:53:35 +0200

    commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,22 +6,14 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getStorage, provideStorage } from '@angular/fire/storage';
-// import { environment } from '../environments/environment.development';
+import { environment } from '../environments/environment.development';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCIw_VDLdKIe_07ziFPONisF9RCaRb72ac",
-  authDomain: "da-bubble-697bb.firebaseapp.com",
-  projectId: "da-bubble-697bb",
-  storageBucket: "da-bubble-697bb.appspot.com",
-  messagingSenderId: "177261164734",
-  appId: "1:177261164734:web:c1a9d6e1ba5fe82dd4c6ef"
-};
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     importProvidersFrom(
-      provideFirebaseApp(() => initializeApp(firebaseConfig))
+      provideFirebaseApp(() => initializeApp(environment.firebaseConfig))
     ),
     importProvidersFrom(provideAuth(() => getAuth())),
     importProvidersFrom(provideFirestore(() => getFirestore())),
