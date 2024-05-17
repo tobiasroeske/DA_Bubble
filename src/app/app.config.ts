980ec4df63abcefe8ff6a4e75c41831cd8887027
Tobias Roeske <122990092+tobiasroeske@users.noreply.github.com>
2024-05-17 11:04:45 +0200

    firebase installed

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,21 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { environment } from '../environments/environment.development';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(
+      provideFirebaseApp(() => initializeApp(environment.firebaseConfig))
+    ),
+    importProvidersFrom(provideAuth(() => getAuth())),
+    importProvidersFrom(provideFirestore(() => getFirestore())),
+    importProvidersFrom(provideStorage(() => getStorage())),
+  ],
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    firebaseConfig: {
+        apiKey: "AIzaSyCIw_VDLdKIe_07ziFPONisF9RCaRb72ac",
+        authDomain: "da-bubble-697bb.firebaseapp.com",
+        projectId: "da-bubble-697bb",
+        storageBucket: "da-bubble-697bb.appspot.com",
+        messagingSenderId: "177261164734",
+        appId: "1:177261164734:web:c1a9d6e1ba5fe82dd4c6ef"
+      }
+};
