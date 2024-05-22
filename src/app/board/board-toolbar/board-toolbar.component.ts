a546263f4b51d5048b9356265a8d3c95c5b2f4f9
Tobias Roeske <122990092+tobiasroeske@users.noreply.github.com>
2024-05-22 09:48:39 +0200

    new code

--- src/app/board/board-toolbar/board-toolbar.component.ts
+++ src/app/board/board-toolbar/board-toolbar.component.ts
@@ -1,9 +1,13 @@
-import { Component, EventEmitter, Output, inject } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BoardService } from '../board.service';
 import { SignupService } from '../../shared/services/signup/signup.service';
 import { ShowProfileDialogComponent } from "../../show-profile-dialog/show-profile-dialog.component";
 import { EditProfileDialogComponent } from "../../edit-profile-dialog/edit-profile-dialog.component";
+import { FirestoreService } from '../../shared/services/firestore-service/firestore.service';
+import { user } from '@angular/fire/auth';
+import { CurrentUser } from '../../shared/interfaces/currentUser.interface';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-board-toolbar',
@@ -15,6 +19,9 @@ import { EditProfileDialogComponent } from "../../edit-profile-dialog/edit-profi
 export class BoardToolbarComponent {
   authService = inject(SignupService);
   boardServ = inject(BoardService);
+  firestoreService = inject(FirestoreService)
+  userList: CurrentUser[] = [];
+  // userListSubscription!: Subscription;
 
   
 }
