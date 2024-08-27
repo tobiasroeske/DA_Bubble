1f0d025ac58b6193ccca8fe1135d1dc4b5950b7c
Tobias Roeske <t.roeske91@gmail.com>
2024-08-27 15:59:51 +0200

    dependency update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, LOCALE_ID, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
